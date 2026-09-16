55d6d56b3cf14fe8e6bf86dba586ebbf7a7df8a1
anup soni <sonianup8965@gmail.com>
2026-09-17 00:52:45 +0530

    hello

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:5002/api";
+  "https://ansoyalintervexa-1.onrender.com/api"
 
 export class ApiClient {
   private getToken(): string | null {
